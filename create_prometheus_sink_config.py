aa6bbf28ad76e903937db5e562f20e3ad1eb6e18
Bert <berthayes@users.noreply.github.com>
2021-05-24 20:10:40 -0500

    Update create_prometheus_sink_config.py

--- create_prometheus_sink_config.py
+++ create_prometheus_sink_config.py
@@ -10,10 +10,10 @@ from configparser import ConfigParser
 host_uri = "http://0.0.0.0:8889/metrics"
 
 
-connect_conf = "my-connect-distributed.properties"
-prom_conf_template = "prometheus_config_template.json"
-prom_generated_config = "prom_config.json"
-yak_conf = "./yak_shaving.conf"
+connect_conf = "/home/ubuntu/iot/my-connect-distributed.properties"
+prom_conf_template = "/home/ubuntu/iot/prometheus_config_template.json"
+prom_generated_config = "/home/ubuntu/iot/prom_config.json"
+yak_conf = "/home/ubuntu/iot/yak_shaving.conf"
 
 with tempfile.NamedTemporaryFile(delete=False, mode='wt') as t:
     t.write('[conf]')
@@ -47,9 +47,7 @@ config_dict = prom_dict['config']
 config_dict['value.converter.schema.registry.url'] = schema_url
 config_dict['key.converter.schema.registry.url'] = schema_url
 config_dict['value.converter.schema.registry.basic.auth.user.info'] = schema_creds
-#config_dict['prom.topics'] = prom_topic
 config_dict['topics'] = kafka_topic
-#config_dict['prom.server.uri'] = prom_server_uri
 config_dict['confluent.topic.bootstrap.servers'] = license_broker
 config_dict['confluent.topic.sasl.jaas.config'] = api_key
 config_dict['sasl.jaas.config'] = api_key
@@ -58,7 +56,6 @@ config_dict['confluent.topic.bootstrap.servers'] = license_broker
 config_dict['producer.bootstrap.servers'] = license_broker
 config_dict['consumer.bootstrap.servers'] = license_broker
 config_dict['consumer.sasl.jaas.config'] = api_key
-#config_dict['producer.sasl.jaas.config'] = api_key
 config_dict['producer.sasl.jaas.config'] = api_key
 config_dict['bootstrap.servers'] = license_broker
 config_dict['prometheus.listener.url'] = host_uri
